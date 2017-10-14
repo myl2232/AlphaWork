562176c51d89e8ba83ca66f96ca35b94699d21e7
myl2232 <153101862@qq.com>
2017-10-14 22:22:43 +0800

    first commit from mac
    first commit from mac

---
+++ AlphaWork/Assets/AlphaWork/Scripts/Event/RefreshMainPosArgs.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using System.Collections;
+using GameFramework.Event;
+
+namespace AlphaWork
+{
+    public class RefreshMainPosArgs : GameEventArgs
+    {
+        public override int Id
+        {
+            get
+            {
+                return (int)EventId.RightClickEvent;
+            }
+        }
+        protected Transform m_HitTransform;
+        public Transform TransCache
+        {
+            get{ return m_HitTransform;}
+            set{ value = m_HitTransform;}
+        }
+        public RefreshMainPosArgs(Transform trans)
+        {
+            m_HitTransform = trans;
+        }
+       
+    }
+}
+
