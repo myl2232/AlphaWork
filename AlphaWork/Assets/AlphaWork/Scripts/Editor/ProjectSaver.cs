0daff3b7795d322aa6d09b4d4b6419ca26947e5f
myl2232 <153101862@qq.com>
2017-11-16 20:25:40 +0800

    把XLua添加进来

--- AlphaWork/Assets/AlphaWork/Scripts/Editor/ProjectSaver.cs
+++ AlphaWork/Assets/AlphaWork/Scripts/Editor/ProjectSaver.cs
@@ -9,7 +9,7 @@ namespace AlphaWork.Editor
         /// 存储可序列化的资源。
         /// </summary>
         /// <remarks>等同于执行 Unity 菜单 File/Save Project。</remarks>
-        [MenuItem("Star Force/Save Assets &s")]
+        [MenuItem("AlphaWork/Save Assets &s")]
         public static void SaveAssets()
         {
 #if UNITY_5_5_OR_NEWER
